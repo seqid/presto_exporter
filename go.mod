0b9160ba6b1f102a6f9317341bd19eaf10c95f50
wenfengsui <tank.sui@shein.com>
2019-04-08 18:45:24 +0800

    presto monitor

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/yahoojapan/presto_exporter
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf
+	github.com/beorn7/perks v0.0.0-20160804104726-4c0e84591b9a
+	github.com/golang/protobuf v0.0.0-20170920220647-130e6b02ab05
+	github.com/matttproud/golang_protobuf_extensions v1.0.1
+	github.com/prometheus/client_golang v0.0.0-20171005112915-5cec1d0429b0
+	github.com/prometheus/client_model v0.0.0-20170216185247-6f3806018612
+	github.com/prometheus/common v0.0.0-20171006141418-1bab55dd05db
+	github.com/prometheus/procfs v0.0.0-20170703101242-e645f4e5aaa8
+	github.com/sirupsen/logrus v0.0.0-20170822132746-89742aefa4b2
+	golang.org/x/crypto v0.0.0-20171023145632-2509b142fb2b
+	golang.org/x/sys v0.0.0-20171023175151-a1a1f1746d15
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
--- presto_exporter.go
+++ presto_exporter.go
@@ -18,21 +18,30 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"io/ioutil"
-	"net/http"
 )
 
 const (
 	namespace = "presto_cluster"
 )
 
+var hostname, _ = os.Hostname()
+
 type Exporter struct {
-	uri              string
+	uri string
+}
+
+type ClusterExporter struct {
 	RunningQueries   float64 `json:"runningQueries"`
 	BlockedQueries   float64 `json:"blockedQueries"`
 	QueuedQueries    float64 `json:"queuedQueries"`
@@ -44,51 +53,94 @@ type Exporter struct {
 	TotalCpuTimeSecs float64 `json:"totalCpuTimeSecs"`
 }
 
+type InfoExporter struct {
+	NodeVersion struct {
+		Version string `json:"version"`
+	} `json:"nodeVersion"`
+	Environment string `json:"environment"`
+	Coordinator bool   `json:"coordinator"`
+	Starting    bool   `json:"starting"`
+	Uptime      string `json:"uptime"`
+}
+
+type Query struct {
+	QueryId    string `json:"queryId"`
+	State      string `json:"state"`
+	Scheduled  bool   `json:"scheduled"`
+	Query      string `json:"query"`
+	QueryStats struct {
+		QueuedTime                string  `json:"queuedTime"`
+		ElapsedTime               string  `json:"elapsedTime"`
+		ExecutionTime             string  `json:"executionTime"`
+		TotalDrivers              int     `json:"totalDrivers"`
+		RawInputDataSize          string  `json:"rawInputDataSize"`
+		CumulativeUserMemory      float64 `json:"cumulativeUserMemory"`
+		PeakUserMemoryReservation string  `json:"peakUserMemoryReservation"`
+		TotalCpuTime              string  `json:"totalCpuTime"`
+		TotalScheduledTime        string  `json:"totalScheduledTime"`
+	} `json:"queryStats"`
+}
+
+type QueryExporter struct {
+	Querys []Query
+}
+
 var (
 	runningQueries = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "running_queries"),
 		"Running requests of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	blockedQueries = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "blocked_queries"),
 		"Blocked queries of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	queuedQueries = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "queued_queries"),
 		"Queued queries of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	activeWorkers = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "active_workers"),
 		"Active workers of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	runningDrivers = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "running_drivers"),
 		"Running drivers of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	reservedMemory = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "reserved_memory"),
 		"Reserved memory of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	totalInputRows = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "total_input_rows"),
 		"Total input rows of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	totalInputBytes = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "total_input_bytes"),
 		"Total input bytes of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
 	)
 	totalCpuTimeSecs = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "total_cpu_time_secs"),
 		"Total cpu time of the presto cluster.",
-		nil, nil,
+		[]string{"hostname"}, nil,
+	)
+	uptime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "uptime"),
+		"Total up time of the presto cluster.",
+		[]string{"hostname"}, nil,
+	)
+
+	querys = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "querys"),
+		"Querys of the presto cluster.",
+		[]string{"hostname", "queryId", "state", "scheduled", "query", "queuedTime", "elapsedTime", "executionTime", "totalDrivers", "rawInputDataSize", "cumulativeUserMemory", "PeakUserMemoryReservation", "totalCpuTime", "totalScheduledTime"}, nil,
 	)
 )
 
@@ -103,6 +155,8 @@ func (e Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- totalInputRows
 	ch <- totalInputBytes
 	ch <- totalCpuTimeSecs
+	ch <- uptime
+	ch <- querys
 }
 
 func main() {
@@ -111,7 +165,7 @@ func main() {
 		metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
 		opts          = Exporter{}
 	)
-	kingpin.Flag("web.url", "Presto cluster address.").Default("http://localhost:8080/v1/cluster").StringVar(&opts.uri)
+	kingpin.Flag("web.url", "Presto cluster address.").Default("http://localhost:8080").StringVar(&opts.uri)
 
 	log.AddFlags(kingpin.CommandLine)
 	kingpin.Version(version.Print("presto_exporter"))
@@ -140,35 +194,107 @@ func main() {
 
 // Collect implements the prometheus.Collector interface.
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	resp, err := http.Get(e.uri)
+	// cluster
+	clusterResp, err := http.Get(e.uri + "/v1/cluster")
 	if err != nil {
 		log.Errorf("%s", err)
 		return
 	}
-	if resp.StatusCode != 200 {
+	if clusterResp.StatusCode != 200 {
 		log.Errorf("%s", err)
 		return
 	}
-	defer resp.Body.Close()
+	defer clusterResp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	clusterBody, err := ioutil.ReadAll(clusterResp.Body)
 	if err != nil {
 		log.Errorf("%s", err)
 		return
 	}
-	err = json.Unmarshal(body, &e)
+	var clusterExporter = ClusterExporter{}
+	err = json.Unmarshal(clusterBody, &clusterExporter)
 	if err != nil {
 		log.Errorf("%s", err)
 		return
 	}
 
-	ch <- prometheus.MustNewConstMetric(runningQueries, prometheus.GaugeValue, e.RunningQueries)
-	ch <- prometheus.MustNewConstMetric(blockedQueries, prometheus.GaugeValue, e.BlockedQueries)
-	ch <- prometheus.MustNewConstMetric(queuedQueries, prometheus.GaugeValue, e.QueuedQueries)
-	ch <- prometheus.MustNewConstMetric(activeWorkers, prometheus.GaugeValue, e.ActiveWorkers)
-	ch <- prometheus.MustNewConstMetric(runningDrivers, prometheus.GaugeValue, e.RunningDrivers)
-	ch <- prometheus.MustNewConstMetric(reservedMemory, prometheus.GaugeValue, e.ReservedMemory)
-	ch <- prometheus.MustNewConstMetric(totalInputRows, prometheus.GaugeValue, e.TotalInputRows)
-	ch <- prometheus.MustNewConstMetric(totalInputBytes, prometheus.GaugeValue, e.TotalInputBytes)
-	ch <- prometheus.MustNewConstMetric(totalCpuTimeSecs, prometheus.GaugeValue, e.TotalCpuTimeSecs)
+	// info
+	infoResp, err := http.Get(e.uri + "/v1/info")
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+	if infoResp.StatusCode != 200 {
+		log.Errorf("%s", err)
+		return
+	}
+	defer infoResp.Body.Close()
+
+	infoBody, err := ioutil.ReadAll(infoResp.Body)
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+	var infoExporter = InfoExporter{}
+	err = json.Unmarshal(infoBody, &infoExporter)
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+
+	// query
+	queryResp, err := http.Get(e.uri + "/v1/query")
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+	if queryResp.StatusCode != 200 {
+		log.Errorf("%s", err)
+		return
+	}
+	defer queryResp.Body.Close()
+
+	queryBody, err := ioutil.ReadAll(queryResp.Body)
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+	var queryExporter = QueryExporter{}
+	err = json.Unmarshal(queryBody, &queryExporter.Querys)
+	if err != nil {
+		log.Errorf("%s", err)
+		return
+	}
+
+	ch <- prometheus.MustNewConstMetric(runningQueries, prometheus.GaugeValue, clusterExporter.RunningQueries, hostname)
+	ch <- prometheus.MustNewConstMetric(blockedQueries, prometheus.GaugeValue, clusterExporter.BlockedQueries, hostname)
+	ch <- prometheus.MustNewConstMetric(queuedQueries, prometheus.GaugeValue, clusterExporter.QueuedQueries, hostname)
+	ch <- prometheus.MustNewConstMetric(activeWorkers, prometheus.GaugeValue, clusterExporter.ActiveWorkers, hostname)
+	ch <- prometheus.MustNewConstMetric(runningDrivers, prometheus.GaugeValue, clusterExporter.RunningDrivers, hostname)
+	ch <- prometheus.MustNewConstMetric(reservedMemory, prometheus.GaugeValue, clusterExporter.ReservedMemory, hostname)
+	ch <- prometheus.MustNewConstMetric(totalInputRows, prometheus.GaugeValue, clusterExporter.TotalInputRows, hostname)
+	ch <- prometheus.MustNewConstMetric(totalInputBytes, prometheus.GaugeValue, clusterExporter.TotalInputBytes, hostname)
+	ch <- prometheus.MustNewConstMetric(totalCpuTimeSecs, prometheus.GaugeValue, clusterExporter.TotalCpuTimeSecs, hostname)
+	uptimeF, _ := strconv.ParseFloat(strings.TrimSuffix(infoExporter.Uptime, "d"), 32)
+	ch <- prometheus.MustNewConstMetric(uptime, prometheus.GaugeValue, uptimeF, hostname)
+
+	for _, v := range queryExporter.Querys {
+		labels := []string{
+			hostname,
+			v.QueryId,
+			v.State,
+			strconv.FormatBool(v.Scheduled),
+			v.Query,
+			v.QueryStats.QueuedTime,
+			v.QueryStats.ElapsedTime,
+			v.QueryStats.ExecutionTime,
+			strconv.Itoa(v.QueryStats.TotalDrivers),
+			v.QueryStats.RawInputDataSize,
+			strconv.FormatFloat(v.QueryStats.CumulativeUserMemory, 'E', -1, 32),
+			v.QueryStats.PeakUserMemoryReservation,
+			v.QueryStats.TotalCpuTime,
+			v.QueryStats.TotalScheduledTime,
+		}
+		ch <- prometheus.MustNewConstMetric(querys, prometheus.GaugeValue, v.QueryStats.CumulativeUserMemory, labels...)
+	}
 }
